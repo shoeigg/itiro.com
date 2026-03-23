ceb7fe5d6b164d22ea19b687ba3636b6dce36b19
aozatou <79564796+shoeigg@users.noreply.github.com>
2026-03-24 06:36:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const hamburger = document.getElementById('hamburger');
     const nav = document.getElementById('nav');
-}
-                          
+
     if (hamburger && nav) {
         hamburger.addEventListener('click', () => {
             nav.classList.toggle('active');
         });
     }
-    
+
     const navLinks = document.querySelectorAll('.nav-list a');
+
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
             if (nav.classList.contains('active')) {
@@ -17,4 +18,5 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+
 });
